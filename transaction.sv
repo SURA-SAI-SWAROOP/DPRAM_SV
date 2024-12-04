3b6f18758f866c969ca80c3dc4821a576ce9d648
SURA SAI SWAROOP <128355949+SURA-SAI-SWAROOP@users.noreply.github.com>
2024-12-04 17:56:38 +0530

    transaction.sv

--- transaction.sv
+++ transaction.sv
@@ -1,19 +1,21 @@
 class transaction;
-rand bit [7:0] data_in;
-rand bit [5:0] wr_addr;
-rand bit [5:0] rd_addr;
-rand bit we;
-rand bit re;
-bit [7:0] data_out;
-constraint read{rd_addr inside {[0:5]};}
-constraint write(wr_addr inside {[0:5]};}
-function transaction copy();
-copy=new();
-copy.data_in=this.data_in;
-copy.wr_addr=this.wr_addr;
-copy.rd_addr=this.rd_addr;
-copy.we=this.we;
-copy.re=this.re;
-copy.data_out=this.data_out;
-endfunction
+  rand bit [7:0] data_in;
+  rand bit [5:0] wr_addr;
+  rand bit [5:0] rd_addr;
+  rand bit we;
+  rand bit re;
+  bit [7:0] data_out;
+  
+  constraint read{rd_addr inside {[0:5]};}
+  constraint write(wr_addr inside {[0:5]};}
+                   
+  function transaction copy();
+    copy=new();
+    copy.data_in=this.data_in;
+    copy.wr_addr=this.wr_addr;
+    copy.rd_addr=this.rd_addr;
+    copy.we=this.we;
+    copy.re=this.re;
+    copy.data_out=this.data_out;
+  endfunction
 endclass
